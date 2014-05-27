33fb28146f5aa9b99a5939ea1b97605d48b6d0c9
Ahmed Arif Khan <khanaahmed10@gmail.com>
2014-05-27 11:55:00 -0400

    Initial Commit

---
+++ UberJump/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  UberJump
+//
+//  Created by Ahmed Arif Khan on 2014-05-27.
+//  Copyright (c) 2014 Ahmed Khan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ UberJumpTests/UberJumpTests.m
@@ -0,0 +1,34 @@
+//
+//  UberJumpTests.m
+//  UberJumpTests
+//
+//  Created by Ahmed Arif Khan on 2014-05-27.
+//  Copyright (c) 2014 Ahmed Khan. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface UberJumpTests : XCTestCase
+
+@end
+
+@implementation UberJumpTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
